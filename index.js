0859969fe7a11dee46ca8d9135ec58ffc3309ed8
Pierson Brannan <plbrannan95@gmail.com>
2019-01-28 13:59:08 -0500

    reverting this trash

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
-app.set("view engine", "pug");
+// app.set("view engine", "pug");
+app.set("view engine", "hbs")
 app.set("views", "./views");
 
 app.use(require("./routes/switchboard"));
