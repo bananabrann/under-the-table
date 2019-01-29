24ee7e0a556d1572a0dd837e48ee89c296ec8ec3
Pierson Brannan <plbrannan95@gmail.com>
2019-01-29 11:31:13 -0500

    added new dependencies

--- controllers/homepage-controller.js
+++ controllers/homepage-controller.js
@@ -2,6 +2,9 @@ const { Game } = require("../models/Game");
 
 module.exports = {
   index: (req, res) => {
-      res.render("index")
+    //   res.render("index")
+    Game.find({}).then(games => {
+        res.render('index', { games })
+    })
   }
 };
