2ebaa12dd622c7476307568e7259cd42818b0287
Pierson Brannan <plbrannan95@gmail.com>
2019-01-29 11:21:19 -0500

    created test debugging on index.hbs

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,9 @@
 
 const mongoose = require("./connection");
 const data = require("./seedData");
-const Game = require("../models/Game").Game;
+const { Game } = require("../models/Game")
+// const Game = require("../models/Game").Game;
 // or
-// const { Game } = require("../models/Game")
 
 Game.collection.insert(data)
+
