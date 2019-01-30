84145666d095222c2efb5ae2486b135c8fe076dc
Pierson Brannan <plbrannan95@gmail.com>
2019-01-30 17:55:18 -0500

    functional comment addition with POST. work on using PUT for comment addition from here on out

--- routes/games.js
+++ routes/games.js
@@ -3,15 +3,14 @@ const router = express.Router();
 
 const gamesController = require("../controllers/games-controller");
 
-router.get("/", gamesController.showAll)
-// console.log("before router tagged")
+router.get("/", gamesController.showAll);
+
 router.get("/new", gamesController.showNewPage);
-// console.log("after router tagged")
 router.post("/new", gamesController.create);
-router.post("/:id", gamesController.addComment)
+
 router.get("/:id", gamesController.showOne);
+router.post("/:id", gamesController.addComment);
 router.put("/:id", gamesController.update);
 router.delete("/:id", gamesController.destroy);
-
 
 module.exports = router;
