147b656898b4aa543139aa8687aa5051a1a71fb8
Pierson Brannan <plbrannan95@gmail.com>
2019-01-30 15:14:37 -0500

    index.hbs limiter added

--- controllers/homepage-controller.js
+++ controllers/homepage-controller.js
@@ -3,8 +3,9 @@ const { Game } = require("../models/Game");
 module.exports = {
   index: (req, res) => {
     //   res.render("index")
-    Game.find({}).then(games => {
-        res.render('index', { games })
-    })
+    Game.find({}).limit(2)
+      .then(games => {
+        res.render("index", { games })
+      })
   }
 };
