d45d175d6daaf997b1f605d9b8f4ca3d962c1f79
Pierson Brannan <plbrannan95@gmail.com>
2019-01-31 10:53:25 -0500

    attempted to make homepage display games in reverse order, didnot work, code hidden but left in

--- controllers/homepage-controller.js
+++ controllers/homepage-controller.js
@@ -2,10 +2,35 @@ const { Game } = require("../models/Game");
 
 module.exports = {
   index: (req, res) => {
+
+    // ATTEMPTING TO DSPLAY NEWEST ADDED GAME WITHOUT ADDING TIME TRACKER TO GAMES
     //   res.render("index")
-    Game.find({}).limit(2)
-      .then(games => {
-        res.render("index", { games })
-      })
+    // function printReverse() {
+    //   let gameArray = Game.find({})
+      
+    //       console.log(gameArray[0]);
+
+
+        // res.render("index", {})
+
+      //     for (i = 2; i > 0; i--) {
+      //     }
+      // }
+
+      // for (i = 2; i > 0; i--) {
+      //   Game.findOne({}).limit(1);
+      // }
+    // }
+    
+
+        
+
+      // WORKING:
+      Game.find({})
+        .limit(2)
+        .then(games => {
+          res.render("index", { games });
+        });
+    
   }
 };
